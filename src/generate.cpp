6f2c2c1cf1a753dc3b723cb538d4cd23fcc7279a
Brian Faubion <powerinblack@gmail.com>
2019-04-20 18:04:12 -0400

    Added Perlin-Notch generator, improved Perlin-octave generator

--- src/generate.cpp
+++ src/generate.cpp
@@ -5,11 +5,31 @@ void perlinOctaves(Heightmap<uint16_t>& map, uint16_t min, uint16_t max, unsigne
 {
 	// Create the gradient grid
 	GradientGrid grad(grid_size, grid_size, seed);
+	unsigned width = map.getWidthX();
+	unsigned height = map.getWidthY();
+
+	// Check input values
+	if (grid_size < 2)
+	{
+		grid_size = 2;
+	}
+	if (octaves < 1)
+	{
+		octaves = 1;
+	}
+	if (persistence < 0.0f)
+	{
+		persistence = 0.0f;
+	}
+	else if (persistence > 1.0f)
+	{
+		persistence = 1.0f;
+	}
 
 	// Apply noise
-	for (unsigned y = 0; y < map.getWidthY(); ++y)
+	for (unsigned y = 0; y < height; ++y)
 	{
-		for (unsigned x = 0; x < map.getWidthX(); ++x)
+		for (unsigned x = 0; x < width; ++x)
 		{
 			// Generate multiple layers of noise
 			float noise = 0.0f;
@@ -17,8 +37,8 @@ void perlinOctaves(Heightmap<uint16_t>& map, uint16_t min, uint16_t max, unsigne
 			float total_amplitude = 0.0f;
 
 			// Set the frequency so that the map coordinates will never be outside the grid
-			float fx = (1.0f / (float)pow(2, octaves - 1)) * ((float)(grid_size - 1) / (map.getWidthX() + 2));
-			float fy = (1.0f / (float)pow(2, octaves - 1)) * ((float)(grid_size - 1) / (map.getWidthY() + 2));
+			float fx = (1.0f / (float)pow(2, octaves - 1)) * ((float)(grid_size - 1) / (width + 2));
+			float fy = (1.0f / (float)pow(2, octaves - 1)) * ((float)(grid_size - 1) / (height + 2));
 			
 			for (unsigned o = 0; o < octaves; ++o)
 			{
@@ -35,7 +55,57 @@ void perlinOctaves(Heightmap<uint16_t>& map, uint16_t min, uint16_t max, unsigne
 	}
 }
 
-void octavePerlinF(Heightmap<uint16_t>& map, unsigned seed)
+void perlinNotch(Heightmap<uint16_t>& map, uint16_t min, uint16_t max, unsigned seed, unsigned grid_size, float roughness, float detail)
+{
+	// Create the gradient grid
+	GradientGrid grad1(grid_size, grid_size, seed);
+	GradientGrid grad2(grid_size, grid_size, seed);
+	GradientGrid grad3(grid_size, grid_size, seed);
+	unsigned width = map.getWidthX();
+	unsigned height = map.getWidthY();
+
+	// Check input values
+	if (grid_size < 2)
+	{
+		grid_size = 2;
+	}
+	if (detail < 0.0f)
+	{
+		detail = 0.0f;
+	}
+	else if (detail > 1.0f)
+	{
+		detail = 1.0f;
+	}
+	if (roughness < 0.0f)
+	{
+		roughness = 0.0f;
+	}
+	else if (roughness > 1.0f)
+	{
+		roughness = 1.0f;
+	}
+
+	// Set the frequency of the heightmaps to avoid going outside of the gradient grid
+	float fx = (float)(grid_size - 1) / (width + 2);
+	float fy = (float)(grid_size - 1) / (height + 2);
+
+	// Apply noise
+	float elevation, rough, details;
+	for (unsigned y = 0; y < height; ++y)
+	{
+		for (unsigned x = 0; x < width; ++x)
+		{
+			elevation = perlin(grad1, (x + 1) * roughness * fx, (y + 1) * roughness * fy);
+			rough = perlin(grad2, (x + 1) * roughness * fx, (y + 1) * roughness * fy);
+			details = perlin(grad3, (x + 1) * fx, (y + 1) * fy);
+
+			map.setHeight(x, y, (uint16_t)(((elevation + rough * details * detail) / 4.0f + 0.5f) * (max - min)) + min);
+		}
+	}
+}
+
+void testperlin(Heightmap<uint16_t>& map, unsigned seed)
 {
 	// Create the gradient grid
 	std::vector<int> p;
--- src/generate.h
+++ src/generate.h
@@ -3,14 +3,27 @@
 #include "heightmap.h"
 
 /*
- * Generate a heightmap using multiple layers of perlin noise stacked on top of one another
+ * Generate a heightmap using multiple layers of Perlin noise stacked on top of one another
  * 
  * min:			The minimum elevation of the heightmap produced
- * max:			The minimum elevation of the heightmap produced
+ * max:			The maximum elevation of the heightmap produced
  * seed:		The rng seed to use for noise generation
- * grid_size:	The size of the gradient grid used for noise generation - smaller grids mean smoother noise, while larger grids will be granier with larger elevation changes (must be > 2)
+ * grid_size:	The size of the gradient grid used for noise generation - smaller grids mean smoother noise, while larger grids will be granier with steep elevation changes (must be > 2)
  * octaves:		The number of layers of noise to use - lower octaves make heightmaps very rough and bumpy, while higher octaves are softer and more homogeneous (must be > 0)
  * persistence:	The level of influence each successive octave has - higher persistence results in bumpier terrain, while lower persistence creates smooth hills (must be between 0.0 and 1.0)
  * 
  */
 void perlinOctaves(Heightmap<uint16_t>& map, uint16_t min, uint16_t max, unsigned seed, unsigned grid_size, unsigned octaves, float persistence);
+
+/*
+ * Generate a heightmap using three layers of Perlin noise, similar to early versions of Minecraft
+ *
+ * min:			The minimum elevation of the heightmap produced
+ * max:			The maximum elevation of the heightmap produced
+ * seed:		The rng seed to use for noise generation
+ * grid_size:	The size of the gradient grid used for noise generation - smaller grids mean smoother noise, while larger grids will be granier with steep elevation changes (must be > 2)
+ * roughness:	Affects the roughness of the terrain - low roughness produces hilly terrain, and high roughness produces more mountainous terrain (must be between 0.0 and 1.0)
+ * detail:		Affects the minor details of the terrain - low details means smooth hills, while higher details means bumpy terrain (must be between 0.0 and 1.0)
+ *
+ */
+void perlinNotch(Heightmap<uint16_t>& map, uint16_t min, uint16_t max, unsigned seed, unsigned grid_size, float roughness, float detail);
